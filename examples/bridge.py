53b50babb16870002b5aef7664a2c056fb91ce6f
yinyi <277383849@qq.com>
2018-08-16 01:37:54 +0800

    hh

---
+++ examples/bridge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    python_pattern.examples.adapter
+    ~~~~~~~~~~~
+
+    适配器模式
+
+    将一个类的接口转换成客户希望的另外一个接 。 Adapter模式使得原本由于接口不兼容而不能一起工作的那些类可以一起工作。
+
+    :copyright: (c) 2017 by the yinyi.
+    :license: BSD, see LICENSE for more details.
+"""
